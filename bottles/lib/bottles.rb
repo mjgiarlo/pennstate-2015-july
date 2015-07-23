b259c8e70b8eab210dac279c9073f1cb5d80fe0a
Michael J. Giarlo <leftwing@alumni.rutgers.edu>
2015-07-23 13:23:41 -0400

    done

--- bottles/lib/bottles.rb
+++ bottles/lib/bottles.rb
@@ -1,5 +1,15 @@
-class Bottles
+class Fixnum
+  def to_bottle_number
+    klass = Object.const_get("Container#{self}") rescue ContainerNumber
+    klass.new(self)
+  end
 
+  def pred
+    self == 0 ? 99 : self - 1
+  end
+end
+
+class Bottles
   def song
     verses(99, 0)
   end
@@ -9,16 +19,76 @@ class Bottles
   end
 
   def verse(number)
-    case number
-    when 0
-      "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    when 1
-      "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n"
-    else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\nTake one down and pass it around, #{number-1} bottles of beer on the wall.\n"
-    end
+    puts "number is #{number}"
+    container_number = number.to_bottle_number
+
+    "#{container_number} of beer on the wall, ".capitalize +
+      "#{container_number} of beer.\n" +
+      "#{container_number.phrase}, " +
+      "#{container_number.successor} of beer on the wall.\n"
+  end
+end
+
+class ContainerNumber
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
   end
 
+  def to_s
+    "#{quantity} #{container}"
+  end
+
+  def phrase
+    "Take #{pronoun} down and pass it around"
+  end
+
+  def pronoun
+    "one"
+  end
+
+  def container
+    "bottles"
+  end
+
+  def quantity
+    number.to_s
+  end
+
+  def successor
+    (number - 1).to_bottle_number
+  end
 end
+
+class Container0 < ContainerNumber
+  def phrase
+    "Go to the store and buy some more"
+  end
+
+  def quantity
+    "no more"
+  end
+end
+
+class Container1 < ContainerNumber
+  def pronoun
+    "it"
+  end
+
+  def container
+    "bottle"
+  end
+end
+
+class Container6 < ContainerNumber
+  def quantity
+    'one'
+  end
+
+  def container
+    'six-pack'
+  end
+end
+
+puts Bottles.new.song
